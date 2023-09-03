fa672435b253b44e3828c8a579b044f2f21e4e81
brunamv <brumvienna@hotmail.com>
2023-09-03 20:57:57 -0300

    Spring Data

--- src/main/java/br/com/fiap/SpotyFilmes/controllers/CategoriaController.java
+++ src/main/java/br/com/fiap/SpotyFilmes/controllers/CategoriaController.java
@@ -2,6 +2,7 @@ package br.com.fiap.SpotyFilmes.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -37,10 +38,7 @@ public class CategoriaController {
     @GetMapping("/categorias/{id}")
     public ResponseEntity<Categoria> readCategoria(@PathVariable Long id) {
         log.info("Exibindo a Categoria de ID: " + id);
-        var categoria_buscada = categoriaRepository.findById(id);
-        if (categoria_buscada.isEmpty()) 
-            return ResponseEntity.notFound().build();
-        return ResponseEntity.ok(categoria_buscada.get());
+        return ResponseEntity.ok(getCategoriaById(id));
     }
 
     @PostMapping("/categorias")
@@ -53,25 +51,20 @@ public class CategoriaController {
     @PutMapping("/categorias/{id}")
     public ResponseEntity<Categoria> updateCategoria(@PathVariable Long id, @RequestBody Categoria categoria_atualizar){
         log.info("Atualizando a Categoria de ID: " + id);
-        var categoria_buscada = categoriaRepository.findById(id);
-        if (categoria_buscada.isEmpty())
-            return ResponseEntity.notFound().build();
-        
-        categoria_buscada.get().setNome(categoria_atualizar.getNome());
-        categoria_buscada.get().setUrl_imagem(categoria_atualizar.getUrl_imagem());
-        categoria_buscada.get().setDescricao(categoria_atualizar.getDescricao());
-        categoria_buscada.get().setClassificacao(categoria_atualizar.getClassificacao());
-        categoriaRepository.save(categoria_buscada.get());
-        return ResponseEntity.ok(categoria_buscada.get()); 
+        getCategoriaById(id);
+        categoria_atualizar.setId(id);
+        categoriaRepository.save(categoria_atualizar);
+        return ResponseEntity.ok(categoria_atualizar);
     }
 
     @DeleteMapping("/categorias/{id}")
     public ResponseEntity<Categoria> deleteCategoria(@PathVariable Long id) {
         log.info("Deletando a Categoria de ID: " + id);
-        var categoria_buscada = categoriaRepository.findById(id);
-        if (categoria_buscada.isEmpty())
-            return ResponseEntity.notFound().build();
-        categoriaRepository.deleteById(id);
-        return ResponseEntity.noContent().build();        
+        categoriaRepository.delete(getCategoriaById(id));
+        return ResponseEntity.noContent().build();
+    }
+
+    private Categoria getCategoriaById(Long id){
+        return categoriaRepository.findById(id).orElseThrow(() -> {return new RuntimeException();});  
     }
 }
