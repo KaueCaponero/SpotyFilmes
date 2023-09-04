23512647e008622f1b3ecb6496ca19380ef2a202
brunamv <brumvienna@hotmail.com>
2023-09-03 21:00:35 -0300

    correção

--- src/main/java/br/com/fiap/SpotyFilmes/controllers/CategoriaController.java
+++ src/main/java/br/com/fiap/SpotyFilmes/controllers/CategoriaController.java
@@ -2,7 +2,6 @@ package br.com.fiap.SpotyFilmes.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
