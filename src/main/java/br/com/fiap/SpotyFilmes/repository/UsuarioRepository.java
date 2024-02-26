0f988cd325aae000963625638ad3f7f5b45a7b88
KaueCaponero <kauecaponero@hotmail.com>
2024-02-26 09:16:16 -0300

    Ajustes

--- src/main/java/br/com/fiap/SpotyFilmes/repository/UsuarioRepository.java
+++ src/main/java/br/com/fiap/SpotyFilmes/repository/UsuarioRepository.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
     Optional<Usuario> findByEmail(String email);
